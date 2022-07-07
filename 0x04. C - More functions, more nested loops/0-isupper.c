3dc35fa66248d65f885690479b40f76752c3db45
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-07-07 23:23:57 +0200

    0-isupper.c

--- 0x04. C - More functions, more nested loops/0-isupper.c
+++ 0x04. C - More functions, more nested loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-   * main - check the code for Holberton School students.
-    *
-     * Return: Always 0.
-      */
-int main(void)
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
 {
-	    char c;
-
-	        c = 'A';
-		    printf("%c: %d\n", c, _isupper(c));
-		        c = 'a';
-			    printf("%c: %d\n", c, _isupper(c));
-			        return (0);
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
