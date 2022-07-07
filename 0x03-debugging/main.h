a50fc3c251248d0463b29f78cc1c34e7f35fecd7
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-07-07 23:03:44 +0200

    main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef Main_H
+#define Main_H
+
+#include <stdio.h>
+
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+
+#endif /* Main_H */
