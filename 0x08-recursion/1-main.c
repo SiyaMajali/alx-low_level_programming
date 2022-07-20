8e8b01d7741c5b6f2c2d107f8108092e6ee69525
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-07-21 00:34:15 +0200

    1-main.c

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_print_rev_recursion("Corbin Coleman");
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+S
