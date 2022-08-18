9ba9edbe9f2acf762b3abe45de2dfd78af9ee766
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-08-19 00:15:58 +0200

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *clear_bit - clear the value of a bit to 0
+ *@n: unsigned long int*
+ *@index: unsigned int
+ *Return: int
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = 1 << index;
+
+	*n = *n & ~mask;
+
+	return (1);
+
+}
+
