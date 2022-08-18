3f472d4f87665588da7a82e3a902cbb25c041b1b
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-08-19 00:12:38 +0200

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 1 if little, 0 if big
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 0x1234;
+	char *s = (char *)&x;
+
+	if (*s == 0x34)
+		return (1);
+	else
+		return (0);
+
+
+
+
+}
