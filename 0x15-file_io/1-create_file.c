e90afb1bc064423abc6fef54b83f4b0bbb88889b
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-08-23 00:08:09 +0200

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file-create a file
+ * @filename: const char
+ * @text_content: char
+ * Return:int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd;
+	int len;
+	int x;
+
+	if (!filename)
+		return (-1);
+	/*open file*/
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	x = write(fd, text_content, len);
+	close(fd);
+
+	if (x == -1)
+		return (-1);
+
+	return (1);
+
+
+
+}
