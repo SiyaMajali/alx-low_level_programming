0c980fba693ba1817dc1ea12f7548e810e6a0808
Majali D Siyamthanda <74431717+SiyaMajali@users.noreply.github.com>
2022-10-27 04:06:01 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+
+	if (!hash)
+		return (NULL);
+
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!hash->array)
+		return (NULL);
+
+	hash->size = size;
+	while (i < size)
+	{
+		hash->array[i] = NULL;
+		i++;
+	}
+	return (hash);
+}
